1203af93abfb49f43a80c2becc777c45f79816ee
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-09-22 17:31:42 -0700

    Create async.js

---
+++ async.js
@@ -0,0 +1,7 @@
+async function getPost() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');  
+    const data = await response.json();
+    console.log(data);
+ }
+ 
+ getPost();
